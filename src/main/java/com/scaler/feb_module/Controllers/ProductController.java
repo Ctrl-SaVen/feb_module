7d75766ee36978dc842ea9b2bec98fa62068ceea
Ctrl-SaVen <vsaivenkatesh7@gmail.com>
2025-02-06 20:52:20 +0530

    added functions

--- src/main/java/com/scaler/feb_module/Controllers/ProductController.java
+++ src/main/java/com/scaler/feb_module/Controllers/ProductController.java
@@ -1,6 +1,32 @@
 package com.scaler.feb_module.Controllers;
 
+import com.scaler.feb_module.Models.Products;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+
+
+@RequestMapping(value = "/createProduct",method=RequestMethod.POST)
+    public void createProduct(Products products){
+
+    }
+
+    public Products getProductById(long id){
+    return null;
+    }
+
+    public void deleteProductById(long id){
+
+    }
+
+    public void updateProductById(Products product){
+
+    }
+
+
 
 
 
